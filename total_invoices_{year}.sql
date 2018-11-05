4f02fd4a2905ebba4a72d2e11740a19dee7d7625
Taylor Gulley <taylorlgulley@yahoo.com>
2018-11-05 15:46:03 -0600

    added steve's way of doing number 8

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -3,3 +3,14 @@ From Invoice
 Where InvoiceDate LIKE '%2009%'
 OR InvoiceDate LIKE '%2011%'
 ;
+
+/*
+//This is Steve's way
+select count(i.InvoiceId) NumberOfInvoices,
+       strftime('%Y',i.InvoiceDate) as InvoiceYear
+from Invoice i
+where InvoiceYear = '2011'
+or InvoiceYear = '2009'
+group by InvoiceYear
+;
+*/
